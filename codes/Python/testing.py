471e5fff56911c9dbd6f4a8e2a5a4a7366b98a28
prashant_Maurya <prashantmaurya1009@gmail.com>
2021-01-11 13:33:32 +0530

    just commit

--- codes/Python/testing.py
+++ codes/Python/testing.py
@@ -1,4 +1,11 @@
 import re
 valid_char = re.compile('[abcd]+')
 # print(valid_char.match("aaaaaaaaaaaaaaaaa").span()[1])
-print(re.compile("\s+\d+\s[>]").match("\t\t\t19 >").group())
+l = {'fjdks':89,"fjdks":78}
+def name(fun,value):
+    fun(value)
+
+def npass(value):
+    print(value)
+
+name(npass,"prashant")
